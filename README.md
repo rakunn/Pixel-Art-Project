d91564ff8d6bd8d3311bf5b62e3b3eb47c4dd391
rakunn <rbagrowsk@gmail.com>
2018-01-09 21:41:27 +0100

    Namespacing + few enhancements

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Pixel Art Maker Project
 
-This is submission of the final project in Google Developer Challenge.
+## Table of Contents
 
-Simple pixel coloring app, with custom & random colors. Written in JS (ES6) / HTML / CSS and with Semantic UI framework.
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+
+## Instructions
+
+To get started, open `designs.js` and start building out the app's functionality.
+
+For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
--- designs.js
+++ designs.js
@@ -1,154 +1,151 @@
-//few selector variables...
-const userForm = $("#start");
-const colorPick = $("#colorPicker"); 
-const reset = $("#reset");
-const heightInput = $("#input_height");
-const widthInput = $("#input_width");
-const listItems = $("a.item");
-const canvas = $("#pixel_canvas");
-const currentBrush = $("#currentBrush");
+const PixelArt = {
+	currentBrush: $("#currentBrush"),
+	currentColorType: 'custom',
+	isColor: true,
+	init() {
+		this.initializeFormsAndMenu();
+	},
+	initializeFormsAndMenu() {
+		const reset = $("#reset");
+		const listItems = $("a.item");
+		const userForm = $("#start");
+		const colorPick = $("#colorPicker");
+		// When size is submitted by the user, call makeGrid()
+		userForm.on('click', (event) => {
+			event.preventDefault();
+			PixelArt.makeGrid();
+		});
 
-let currentColorType = 'custom';
-let isColor = true;
+		reset.on('click', () => this.resetGrid());
 
-// Select size input
-const getSize = () => {
-	let height = Number(heightInput.val());
-	let width = Number(widthInput.val());
-	if (height > 64) {
-		height = 64;
-		heightInput.val('64');
-	}
-	if (width > 64) {
-		width = 64
-		widthInput.val('64');
-	}
-	return [ height, width ];
-}
+		listItems.on('click', function() {
+			$(this).siblings().removeClass("active");
+			$(this).addClass("active");
+			if(this.dataset.colors) {
+				PixelArt.currentColorType = this.dataset.colors;
+				if(PixelArt.currentColorType === 'custom') {
+					$('#colorInstructions').text('Choose your color');
+					colorPick.show();
+				} else {
+					$('#colorInstructions').text('Random color enabled. Please proceed with your canvas :-)');
+					colorPick.hide();
+				}
+			} else {
+				isDraggable = this.dataset.brush;
+			}
+		});
+		this.currentBrush.parent().popup();
+	},
+	makeGrid() {
+		const canvas = $("#pixel_canvas");
+		const [ height, width ] = PixelArt.getSizeAndSanitizeInput();
+		canvas.find(".row-element").hide(200, () => $(this).remove());
 
-
-//Clear paintings
-const resetGrid = () => {
-	$(".element").css('background-color', 'white');
-}
-
-//Start the fun
-const makeGrid = () => {
-	canvas.find(".row-element").hide(200, () => $(this).remove());
-
-	const [ height, width ] = getSize();
-
-	for (let row = 0; row < height; row++) {
-		canvas.append("<div class='row-element'></div>");
-		for (let col = 0; col < width; col++) {
-			canvas.find(".row-element").last().prepend("<div class='element'></div>");
+		for (let row = 0; row < height; row++) {
+			canvas.append("<div class='row-element'></div>");
+			for (let col = 0; col < width; col++) {
+				canvas.find(".row-element").last().prepend("<div class='element'></div>");
+			}
 		}
-	}
-	colorGrid();
-}
+		this.colorGrid();
+	},
+	resetGrid() {
+		$(".element").css('background-color', 'white');
+	},
+	colorGrid() {
+		const colorPick = $("#colorPicker");
+		const element = $('.element');
+		let isDraggable = false;
 
-const randomColor = () => {
-	if (isColor) {
-		let red = Math.floor(Math.random()*256);
-		let green = Math.floor(Math.random()*256);
-		let blue = Math.floor(Math.random()*256);
-		return `rgb(${red},${green},${blue})`;
+		colorPick.on('change', (event) => {
+			$('i.square.icon').css('color', colorPick.val());
+		});
+
+		element.on('click', function(event) {
+			const target = $(event.target);
+			event.preventDefault();
+			PixelArt.isColor = true;
+			PixelArt.colorOrErase(target);
+		});
+
+		element.on('mousemove', function(event) {
+			const target = $(event.target);
+			event.preventDefault();
+			if(isDraggable) {
+				PixelArt.colorOrErase(target);
+			}
+		});
+
+		element.on('mousedown', function(event){
+			event.preventDefault();
+			isDraggable = true;
+			switch(event.which) {
+				case 1:
+					PixelArt.isColor = true;
+					PixelArt.currentBrush.addClass("paint brush").removeClass("eraser");
+					break;
+				case 3:
+					PixelArt.isColor = false;
+					PixelArt.currentBrush.addClass("eraser").removeClass("paint brush");
+					break;
+			}
+		});
+
+		element.on('contextmenu', function(event){
+			event.preventDefault();
+			PixelArt.isColor = false;
+
+			$(this).css('background-color', PixelArt.customColor());
+		});
+
+		$(document).on('mouseup', function(event){
+			event.preventDefault();
+			PixelArt.currentBrush.addClass("paint brush").removeClass("eraser")
+			isDraggable = false;
+		});
+	},
+	// Select size input
+	getSizeAndSanitizeInput() {
+		let height = $("#input_height").val();
+		let width = $("#input_width").val();
+
+		if (height > 64) {
+			height = 64;
+			$("#input_height").val('64');
+		}
+		if (width > 64) {
+			width = 64
+			$("#input_width").val('64');
+		}
+		return [ height, width ];
+	},
+	randomColor() {
+		if (this.isColor) {
+			let red = Math.floor(Math.random()*256);
+			let green = Math.floor(Math.random()*256);
+			let blue = Math.floor(Math.random()*256);
+			return `rgb(${red},${green},${blue})`;
+			} else {
+			return 'rgb(255,255,255';
+		}
+	},
+	customColor() {
+		if (this.isColor) {
+			return $("#colorPicker").val();
 		} else {
-		return 'rgb(255,255,255';
-	}
-}
-
-const customColor = () => {
-	if (isColor) {
-		return colorPick.val();
-	} else {
-		return 'rgb(255,255,255)';
-	}
-}
-
-const colorOrErase = (targetElement) => {
-	switch(currentColorType) {
-	case 'custom':
-		targetElement.css('background-color', customColor());
-	break;
-	case 'random':
-		targetElement.css('background-color', randomColor());
-	break;
-	}
-}
-
-//event listeners...
-listItems.on('click', function() {
-	$(this).siblings().removeClass("active");
-	$(this).addClass("active");
-	if(this.dataset.colors) {
-		currentColorType = this.dataset.colors;
-		if(currentColorType === 'custom') {
-			$('#colorInstructions').text('Choose your color');
-			$('#colorPicker').show();
-		} else {
-			$('#colorInstructions').text('Random color enabled. Please proceed with your canvas :-)');
-			$('#colorPicker').hide();
+			return 'rgb(255,255,255)';
 		}
-	} else {
-		isDraggable = this.dataset.brush;
+	},
+	colorOrErase(targetElement) {
+		switch(this.currentColorType) {
+		case 'custom':
+			targetElement.css('background-color', PixelArt.customColor());
+		break;
+		case 'random':
+			targetElement.css('background-color', PixelArt.randomColor());
+		break;
+		}
 	}
-});
-
-// When size is submitted by the user, call makeGrid()
-currentBrush.parent().popup();
-
-userForm.on('click', (event) => {
-	event.preventDefault();
-	makeGrid();
-});
-
-reset.on('click', () => resetGrid());
-
-const colorGrid = function() {
-	const element = $('.element');
-	let isDraggable = false;
-
-	element.on('click', function(event) {
-		const target = $(event.target);
-		event.preventDefault();
-		isColor = true;
-		colorOrErase(target);
-	});
-
-	element.on('mousemove', function(event) {
-		const target = $(event.target);
-		event.preventDefault();
-		if(isDraggable) {
-			colorOrErase(target);
-		}
-	});
-
-	element.on('mousedown', function(event){
-		event.preventDefault();
-		isDraggable = true;
-		switch(event.which) {
-			case 1:
-				isColor = true;
-				currentBrush.addClass("paint brush").removeClass("eraser");
-				break;
-			case 3:
-				isColor = false;
-				currentBrush.addClass("eraser").removeClass("paint brush");
-				break;
-		}
-	});
-
-	$(document).on('mouseup', function(event){
-		event.preventDefault();
-		currentBrush.addClass("paint brush").removeClass("eraser")
-		isDraggable = false;
-	});
-
-	element.on('contextmenu', function(event){
-		event.preventDefault();
-		isColor = false;
-
-		$(this).css('background-color', customColor());
-	});
 }
+
+PixelArt.init()
